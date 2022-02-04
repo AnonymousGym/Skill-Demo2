ee2e0fac641174b104ae2e590710ec1d7dc6ed05
AnonymousGym <97484485+AnonymousGym@users.noreply.github.com>
2022-02-04 15:47:02 -0800

    an incorrect test&starter

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,6 @@
-public class SkillDemo{}
+public class SkillDemo{
+
+    public static int minus(int num1, int num2){
+        return num1-num2;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void minusTest(){
+        assertEquals(2, SkillDemo.minus(5, 4));//gonna fail
+    }
+}
