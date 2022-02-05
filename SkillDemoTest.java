8a78891a2062714199ffbedeeb4c97ea10f08fbe
AnonymousGym <97484485+AnonymousGym@users.noreply.github.com>
2022-02-04 16:15:57 -0800

    right version

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void minusTest(){
-        assertEquals(2, SkillDemo.minus(5, 4));//gonna fail
+        assertEquals(2, SkillDemo.minus(5, 3));//gonna succeed
     }
 }
